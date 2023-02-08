28edfd9f766cae3721c50626b4ed6ea56047e5e2
LudvigThunberg <ludvigthunberg@gmail.com>
2023-02-08 10:27:55 +0100

    feat: adds google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
@@ -8,8 +8,21 @@ import { AnimatePresence } from "framer-motion";
 import { Layout } from "../components/basic/Layout";
 import { useRouter } from "next/router";
 
+import * as gtag from "../utils/gtag";
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <RecoilRoot>
       <StaticBackgroundContainer>
